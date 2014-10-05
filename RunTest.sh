dab6d6a56d65fbe534e11e1a0752c3758d820d2b
arvestad <arve@csc.kth.se>
2014-10-05 22:34:47 +0200

    Some perfections

--- RunTest.sh
+++ RunTest.sh
@@ -2,8 +2,14 @@
 
 cd testdata/petimeter
 echo Running petimeter. Test prefix is 't'.
-../../petimeter -n t t t1_contigs.fa t1_cdna.fa t1.gff
-echo Simple table
-../../petimeter_table         t_assessment.json
-echo Producing plots
-../../petimeter_plot testplot t_plotdata.json
+../../petimeter -n t t t1_contigs.fa t1_cdna.fa t1.gff \
+&& echo "--- Simple table ---" \
+&& ../../petimeter_table         t_assessment.json \
+&& echo "--- Agreement ---" \
+&& ../../petimeter_agreement  t_details.json t2_details.json \
+&& echo \
+&& ../../petimeter_agreement -u t_details.json t2_details.json \
+&& echo \
+&& ../../petimeter_agreement -c t_details.json t2_details.json \
+&& echo "--- Producing plots ---"\
+&& ../../petimeter_plot testplot t_details.json
